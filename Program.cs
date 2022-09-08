be0a66d6145cd5a3e99868c48d94780c2a6eee33
Patrick Marques <patrickmarques2009@hotmail.com>
2022-09-08 14:08:08 -0300

    Primeiro commit, início do projeto e criação do menu inicial

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using sistema_de_estacionamento_CSharp.src.Models;
+
+decimal precoInicial;
+decimal precoPorHora;
+
+Console.WriteLine($"Seja bem vindo ao sistema de estacionamento!\n");
+
+Console.WriteLine($"Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"\nAgora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento estacionamento = new Estacionamento();
+
+string opcao;
+bool exibirMenu = true;
+
+while (exibirMenu){
+
+    Console.Clear();
+    Console.WriteLine("Escolha a opção desejada:");
+    Console.WriteLine("1 - Cadastro de veículo");
+    Console.WriteLine("2 - Remoção de veículo");
+    Console.WriteLine("3 - Lista de veículos cadastrados");
+    Console.WriteLine("4 - Encerramento do sistema");
+
+    switch (Console.ReadLine()) {
+
+        case "1":
+        //TO DO
+        Console.WriteLine("Cadastro de veículo acessado");
+        break;
+
+        case "2":
+        // TO DO
+        Console.WriteLine("Remoção de veículo acessado");
+        break;
+
+        case "3":
+        //TO DO
+        Console.WriteLine("Lista de veiculos acessado");
+        break;
+
+        case "4":
+        Console.WriteLine("Sistema encerrado acessado");
+        break;
+
+        default:
+        Console.WriteLine("Opção inválida!");
+        break;
+
+    }
+        
+    Console.WriteLine("Pressione qualquer tecla para continuar");
+    Console.ReadLine();
+
+}
+
+Console.WriteLine("O sistema foi encerrado");
---
+++ src/Models/Estacionamento.cs
@@ -0,0 +1,8 @@
+namespace sistema_de_estacionamento_CSharp.src.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        
+    }
+}
